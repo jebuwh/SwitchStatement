f8ac7d211497d4afcc9c2dd344d964cf99930236
Mona Lisa <email@example.com>
2020-08-25 12:03:46 -0600

    swtcheroo

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            START:
+            Console.WriteLine("What is your favorite elementary school subject?");
+            var subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "Recess":
+                    Console.WriteLine("Yeah man, school is for fools!");
+                    break;
+                case "recess":
+                    Console.WriteLine("Yeah man, school is for fools!");
+                    break;
+                case "nothing":
+                    Console.WriteLine("Yeah man, school is for fools!");
+                    break;
+                case "Nothing":
+                    Console.WriteLine("Yeah man, school is for fools!");
+                    break;
+                default:
+                    Console.WriteLine("Well aren't you a nerd? Try again!");
+                    goto START;
+                    
+            }
+        }
+    }
+}
